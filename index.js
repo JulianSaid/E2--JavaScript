691fe04f4cc4407b9ff459a8b667f677b7c5baf2
Julian Said <juliansaid6@gmail.com>
2023-07-18 21:28:17 -0300

    primer ejercicio

---
+++ index.js
@@ -0,0 +1,67 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+
+const pizzasImpares = pizzas.filter(pizza => {
+  return pizza.id % 2 === 1
+}
+)
+
+pizzasImpares.forEach((pizza) => {
+  console.log(`La ${pizza.nombre} es impar`);
+})
+
+console.log("-------------------------------------");
+
+const pizzaMasBarata = ((precio) => {
+  return pizzas.some(pizzas.precio < precio)
+});
+
+console.log(pizzaMasBarata(800));
